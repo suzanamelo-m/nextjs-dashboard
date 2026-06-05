7af400d4ddc23e07426c80a2a899d8e8142f1b0e
Suzana Melo <suzanamelojor@gmail.com>
2026-06-04 22:09:23 -0300

    nextjs-dashboard: test the database by running a query to check if everything is working as expected

--- app/query/route.ts
+++ app/query/route.ts
@@ -1,26 +1,24 @@
-// import postgres from 'postgres';
+import postgres from "postgres";
 
-// const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
+// Test component to execute a query and check if everything is working as expected after seeding the database
 
-// async function listInvoices() {
-// 	const data = await sql`
-//     SELECT invoices.amount, customers.name
-//     FROM invoices
-//     JOIN customers ON invoices.customer_id = customers.id
-//     WHERE invoices.amount = 666;
-//   `;
+const sql = postgres(process.env.POSTGRES_URL!, { ssl: "require" });
 
-// 	return data;
-// }
+async function listInvoices() {
+  const data = await sql`
+    SELECT invoices.amount, customers.name
+    FROM invoices
+    JOIN customers ON invoices.customer_id = customers.id
+    WHERE invoices.amount = 666;
+  `;
+
+  return data;
+}
 
 export async function GET() {
-  return Response.json({
-    message:
-      'Uncomment this file and remove this line. You can delete this file when you are finished.',
-  });
-  // try {
-  // 	return Response.json(await listInvoices());
-  // } catch (error) {
-  // 	return Response.json({ error }, { status: 500 });
-  // }
+  try {
+    return Response.json(await listInvoices());
+  } catch (error) {
+    return Response.json({ error }, { status: 500 });
+  }
 }
