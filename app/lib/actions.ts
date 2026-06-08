30df9735669fb2c83dba8ed773e7c17283eb333b
Suzana Melo <suzanamelojor@gmail.com>
2026-06-07 22:14:44 -0300

    nextjs-dashboard: update zod to validate errors, create a State type and update CreateInvoice to validate fields before send data to the database

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -5,50 +5,76 @@ import postgres from "postgres";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 
+export type State = {
+  errors?: {
+    customerId?: string[];
+    amount?: string[];
+    status?: string[];
+  };
+  message?: string | null;
+};
+
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: "require" });
 
 // validate data using Zod
 const FormSchema = z.object({
   id: z.string(),
-  customerId: z.string(),
-  amount: z.coerce.number(),
-  status: z.enum(["pending", "paid"]),
+  customerId: z.string({
+    invalid_type_error: "Please select a customer.",
+  }),
+  amount: z.coerce
+    .number()
+    .gt(0, { message: "Please enter an amount greater than $0." }),
+  status: z.enum(["pending", "paid"], {
+    invalid_type_error: "Please select an invoice status.",
+  }),
   date: z.string(),
 });
 
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
-export async function createInvoice(formData: FormData) {
-  const { customerId, amount, status } = CreateInvoice.parse({
+export async function createInvoice(prevState: State, formData: FormData) {
+  // Validate form using Zod
+  const validatedFields = CreateInvoice.safeParse({
     customerId: formData.get("customerId"),
     amount: formData.get("amount"),
     status: formData.get("status"),
   });
+
+  // If form validation fails, return errors early. Otherwise, continue.
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: "Missing Fields. Failed to Create Invoice.",
+    };
+  }
+
+  // Prepare data for insertion into the database
+  const { customerId, amount, status } = validatedFields.data;
+
   // convert the amount into cents
+  // It's a good practice to store monetary values in cents in your database to eliminate JavaScript floating-point errors and ensure accuracy.
   const amountInCents = amount * 100;
-  // amountInCents = It's a good practice to store monetary values in cents in your database to eliminate JavaScript floating-point errors and ensure accuracy.
 
   // create a new date with the format "YYYY-MM-DD"
   const date = new Date().toISOString().split("T")[0];
 
-  // create an SQL query to insert the new invoice into the database
+  // SQL query to insert the new invoice into the database
   try {
     await sql`
       INSERT INTO invoices (customer_id, amount, status, date)
       VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
     `;
   } catch (error) {
-    // We'll also log the error to the console for now
-    console.error(error);
+    // If a database error occurs, return a more specific error.
     return {
       message: "Database Error: Failed to Create Invoice.",
     };
   }
 
-  // revalidate the path, clear the cache and trigger a new request to the server
+  // Revalidate the cache for the invoices page and redirect the user.
   revalidatePath("/dashboard/invoices");
-  // redirect the user back to the /dashboard/invoices page
   redirect("/dashboard/invoices");
 }
 
